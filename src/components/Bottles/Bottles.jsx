69b3d5474338a85b34727548cef651c0aabea1c1
mdalihasanriyad <ahriyad.com@gmail.com>
2026-03-01 19:36:53 +0600

    localstorage setup done

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,35 +1,57 @@
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
-import { addToStoredCart } from "../../utilits/localstorage";
+import { addToStoredCart,getStoreCart } from "../../utilits/localstorage";
 
+const Bottles = ({ bottlePromise }) => {
+      const [cart, setCart] = useState([]);
+  const bottles = use(bottlePromise);
 
-const Bottles = ({bottlePromise}) => {
+ useEffect(() => {
+        const storedCartIds = getStoreCart();
+        // console.log(storedCartIds, bottles);
 
-    const bottles = use(bottlePromise);
-    const [cart, setCart] = useState([]);
+        const storedCart = [];
 
-    const handleAddToCart = (bottle) => {
-       console.log('bottle added', bottle);
-       const newCart = [...cart, bottle];
-         setCart(newCart);
-         addToStoredCart(bottle.id);
-    }
-
-console.log(bottles);
-    return (
-        <div className="max-w-7xl mx-auto space-y-6">
-            <h1>Bottles : {bottles.length}</h1>
-            <p>Cart Items: {cart.length}</p>
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-5 justify-center items-center ">
-                {
-                bottles.map(bottle=><Bottle 
-                    key={bottle.id} 
-                    bottle={bottle}
-                    handleAddToCart={handleAddToCart}></Bottle>)
+        for (const id of storedCartIds) {
+            // console.log(id);
+            const cartBottle = bottles.find(bottle => bottle.id === id);
+            if (cartBottle) {
+                storedCart.push(cartBottle);
             }
-            </div>
-        </div>
-    );
+        }
+
+        console.log('stored cart', storedCart);
+        setCart(storedCart);
+        // 8 no video
+
+    }, [bottles])
+
+
+
+
+  const handleAddToCart = (bottle) => {
+    console.log("bottle added", bottle);
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+    addToStoredCart(bottle.id);
+  };
+
+  console.log(bottles);
+  return (
+    <div className="max-w-7xl mx-auto space-y-6">
+      <h1>Bottles : {bottles.length}</h1>
+      <p>Cart Items: {cart.length}</p>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-5 justify-center items-center ">
+        {bottles.map((bottle) => (
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+          ></Bottle>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Bottles;
