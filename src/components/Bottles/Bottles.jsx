c97bee5d85a720fab2ec4e686febfc4a20c0cdb7
mdalihasanriyad <ahriyad.com@gmail.com>
2026-03-01 19:17:35 +0600

    added localstorage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,5 +1,6 @@
 import { use, useState } from "react";
 import Bottle from "../Bottle/Bottle";
+import { addToStoredCart } from "../../utilits/localstorage";
 
 
 const Bottles = ({bottlePromise}) => {
@@ -9,13 +10,17 @@ const Bottles = ({bottlePromise}) => {
 
     const handleAddToCart = (bottle) => {
        console.log('bottle added', bottle);
+       const newCart = [...cart, bottle];
+         setCart(newCart);
+         addToStoredCart(bottle.id);
     }
 
 console.log(bottles);
     return (
         <div className="max-w-7xl mx-auto space-y-6">
             <h1>Bottles : {bottles.length}</h1>
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center ">
+            <p>Cart Items: {cart.length}</p>
+            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-5 justify-center items-center ">
                 {
                 bottles.map(bottle=><Bottle 
                     key={bottle.id} 
